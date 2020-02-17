267d2d81bda0c80a4d2199665c06a6848c439a68
tjoseph <tjoseph.ubc@gmail.com>
2020-02-17 02:18:05 -0800

    updated

---
+++ src/training_utils/twopoint_max_marginsplit.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def twoPointMaxMarginSplit(X, Y, tol):
+    """
+    This should only be done if X has exactly 2 unique rows in which case it
+    produces the optimal split
+    """
+    bType1 = np.all(np.absolute(np.subtract(X, X[0,:])) < tol, axis=1)
+    YLeft  = Y[bType1,:]
+    YRight = Y[~bType1,:]
+
+    if np.all(YLeft.sum(axis=0) ==  YRight.sum(axis=0)):
+        # Here the two unique points are identical and so we can't helpfully split
+        bSp = False
+        rmm = []
+        cmm = []
+        return bSp, rmm, cmm
+    else:
+        bSp = True;
+
+    # Otherwise the optimal spliting plane is the plane perpendicular
+    # to the vector between the two points (rmm) and the maximal
+    # marginal split point (cmm) is halway between the two points on this line.
+    iType2 = (~bType1).ravel().nonzero()[0][0]
+    rmm    = (X[iType2,:] - X[0,:])[np.newaxis].T
+    cmm    = 0.5 * (X[iType2,:] @ rmm + X[0,:] @ rmm)
+
+    if np.any(np.isnan(cmm)) or np.any(np.isinf(cmm)):
+        assert (True), 'Suggested split point at infitity or NaN!'
+
+    return bSp, rmm, cmm
